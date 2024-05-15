7adc3921822df03fce20e2a5a9471dba1a9ce239
Michał Kopczewski <kopczewski.michal101@gmail.com>
2024-05-15 11:59:45 +0200

    lab 8 zad 1 (kubernetes)

---
+++ lab8/zad1/app/app.py
@@ -0,0 +1,1 @@
+print("Siema, działam")
---
+++ lab8/zad1/index.bash
@@ -0,0 +1,11 @@
+kubectl delete -f my-app-deployment.yaml
+kubectl apply -f my-app-deployment.yaml
+sleep 2
+kubectl get pods
+sleep 2
+kubectl get pods
+sleep 2
+kubectl get pods
+sleep 2
+kubectl get pods
+# kubectl describe pod my-pod
